53f081d84237cdca0fc7780546cd3589a9fcb271
Pavel <rogozhin.p@yandex.ru>
2019-12-07 17:42:39 +0300

    create uniq Project ID exception

--- Java/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ Java/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,14 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project) {
-
-        // Logic
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIndentifier(project.getProjectIndentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '"+
+                    project.getProjectIndentifier().toUpperCase()
+                    +"' already exists");
+        }
     }
 }

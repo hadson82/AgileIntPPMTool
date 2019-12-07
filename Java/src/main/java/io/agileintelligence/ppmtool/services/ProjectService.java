c827ba7fb71d256c5dde497f0e9b06d88727ab17
Pavel <rogozhin.p@yandex.ru>
2019-12-07 18:46:06 +0300

    add find Project by ID and No Project Exception if wrong ID

--- Java/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ Java/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -22,4 +22,15 @@ public class ProjectService {
                     +"' already exists");
         }
     }
+
+    public Project findProjectByIndentifier(String projectId) {
+        Project project = projectRepository.findByProjectIndentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID '"
+                    + projectId + "' does not exist");
+        }
+
+        return project;
+    }
 }
